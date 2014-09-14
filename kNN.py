6026b10222ad8af5c041c3cd8b5a75e48ac70e1b
Jesus Magana <maganazero5@gmail.com>
2014-09-13 21:01:55 -0700

    Added a normalizer function and a test for it

--- kNN.py
+++ kNN.py
@@ -35,3 +35,13 @@ def fileToMatrix(filename):
 		classLabelVector.append(int(listFromLine[-1]))
 		index += 1
 	return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+	minVals = dataSet.min(0)
+	maxVals = dataSet.max(0)
+	ranges = maxVals - minVals
+	normDataSet = zeros(shape(dataSet))
+	m = dataSet.shape[0]
+	normDataSet = dataSet - tile(minVals, (m,1))
+	normDataSet = normDataSet/tile(ranges, (m,1))
+	return normDataSet, ranges, minVals
--- kNNTest.py
+++ kNNTest.py
@@ -19,3 +19,9 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.scatter(datingDataMat[:,1], datingDataMat[:,2],15.0*array(datingLabels), 15.0*array(datingLabels))
 plt.show()
+
+normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
+
+print normMat
+print ranges
+print minVals
