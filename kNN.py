4102b6b2e82deae95988e6bc94db1d3a03624111
Jesus Magana <maganazero5@gmail.com>
2014-09-13 21:22:18 -0700

    Made error test and can run it

--- kNN.py
+++ kNN.py
@@ -45,3 +45,16 @@ def autoNorm(dataSet):
 	normDataSet = dataSet - tile(minVals, (m,1))
 	normDataSet = normDataSet/tile(ranges, (m,1))
 	return normDataSet, ranges, minVals
+
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat,datingLabels = fileToMatrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:], datingLabels[numTestVecs:m],3)
+		print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+		if (classifierResult != datingLabels[i]): errorCount += 1.0
+	print "the total error rate is: %f" % (errorCount/float(numTestVecs))
--- kNNTest.py
+++ kNNTest.py
@@ -25,3 +25,5 @@ normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
 print normMat
 print ranges
 print minVals
+
+kNN.datingClassTest()
